d12bec2423fd6948dd10875c75446f84e7728250
ah-james <11ahj11@gmail.com>
2020-10-25 11:20:25 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+    x = [] # creates an empty array
+    counter = 0
+    while counter < array.length # iterates over array while counter is less than array length
+        x << yield(array[counter]) # pushes the yielded value of the array @ counter # index
+        counter += 1 # adds one to counter at end of iteration
+    end
+    x
+end
 
